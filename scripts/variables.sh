eaf6fe11494f600d3dafd4a358ec4926a6e721a8
Jonathan Duggan <jonathan.s.duggan@gmail.com>
2019-04-03 06:33:59 -0400

    must export variables

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -3,9 +3,9 @@
 ## Fill out required variables and then source this
 ## file when interacting with AWS and/or K8s.
 
-EDITOR=vim
-KOPS_STATE_STORE=
-AWS_DEFAULT_REGION=us-east-1
-AWS_DEFAULT_PROFILE=
-AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id --profile ${AWS_DEFAULT_PROFILE})
-AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key --profile ${AWS_DEFAULT_PROFILE})
+export EDITOR=vim
+export KOPS_STATE_STORE=
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_DEFAULT_PROFILE=
+export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id --profile ${AWS_DEFAULT_PROFILE})
+export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key --profile ${AWS_DEFAULT_PROFILE})
